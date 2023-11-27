3b2383c4a702f41229db61753b75597fc9a6e679
JasurDeveloperZamonov <asadbekzamonov2000@gmail.com>
2023-11-27 10:47:01 +0500

    Xato bartaraf qilindi

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         MathFunction mathFunction=new MathFunction();
-        System.out.print("Birinchi sonni kiriting: ");
-        mathFunction.a=scanner.nextDouble();
-        System.out.print("Ikkinchi sonni kiriting: ");
-        mathFunction.b=scanner.nextDouble();
-
-        System.out.println(mathFunction.mathAdd());
-        System.out.println(mathFunction.mathSub());
-        System.out.println(mathFunction.mathMultiply());
-        System.out.println(mathFunction.mathDiv());
-        System.out.println(mathFunction.mathABS());
-        System.out.println(mathFunction.mathPow());
+//        System.out.print("Birinchi sonni kiriting: ");
+//        mathFunction.a=scanner.nextDouble();
+//        System.out.print("Ikkinchi sonni kiriting: ");
+//        mathFunction.b=scanner.nextDouble();
+        System.out.println(mathFunction.mathAdd(6,7.5));
+        System.out.println(mathFunction.mathSub(7.5,-5));
+        System.out.println(mathFunction.mathMultiply(15,3));
+        System.out.println(mathFunction.mathDiv(-3.6,3));
+        System.out.println(mathFunction.mathABS(-125.6));
+        System.out.println(mathFunction.mathPow(-20));
     }
 }
--- src/MathFunction.java
+++ src/MathFunction.java
@@ -1,24 +1,122 @@
 public class MathFunction {
-    public double a;
-    public double b;
 
-
-    public double mathAdd(){
+    //***************** YIG'INDI ***************************//
+    public double mathAdd(double a,double b){
         return a+b;
     }
-    public double mathSub(){
+    public int mathAdd(int a,int b){
+        return a+b;
+    }
+    public short mathAdd(short a,short b){
+        return (short) (a+b);
+    }
+    public float mathAdd(float a,float b){
+        return a+b;
+    }
+    public long mathAdd(long a,long b){
+        return  a+b;
+    }
+    public byte mathAdd(byte a,byte b){
+        return (byte) (a+b);
+    }
+
+    //***************** AYIRMA ***************************//
+    public double mathSub(double a,double b){
         return a-b;
     }
-    public double mathMultiply(){
+    public int mathSub(int a,int b){
+        return a-b;
+    }
+    public float mathSub(float a,float b){
+        return a-b;
+    }
+    public short mathSub(short a,short b){
+        return (short) (a-b);
+    }
+    public long mathSub(long a,long b){
+        return a-b;
+    }
+    public byte mathSub(byte a,byte b){
+        return (byte) (a-b);
+    }
+
+    //***************** KO'PAYTMA ***************************//
+    public int mathMultiply(int a,int b){
         return a*b;
     }
-    public double mathDiv(){
+    public double mathMultiply(double a,double b){
+        return a*b;
+    }
+    public float mathMultiply(float a, float b){
+        return a*b;
+    }
+    public short mathMultiply(short a, short b){
+        return (short) (a*b);
+    }
+    public byte mathMultiply(byte a, byte b){
+        return (byte) (a*b);
+    }
+    public long mathMultiply(long a, long b){
+        return a*b;
+    }
+    //***************** BO'LINMA ***************************//
+    public double mathDiv(double a,double b){
         return a/b;
     }
-    public double mathABS(){
+    public int mathDiv(int a,int b)
+    {
+        return a/b;
+    }
+    public float mathDiv(float a,float b)
+    {
+        return a/b;
+    }
+    public short mathDiv(short a,short b)
+    {
+        return (short) (a/b);
+    }
+    public byte mathDiv(byte a,byte b)
+    {
+        return (byte) (a/b);
+    }
+    public long mathDiv(long a,long b)
+    {
+        return a/b;
+    }
+
+    //***************** MODUL ***************************//
+    public double mathABS(double a){
         return Math.abs(a);
     }
-    public double mathPow(){
+    public int mathABS(int a){
+        return Math.abs(a);
+    }
+    public float mathABS(float a){
+        return Math.abs(a);
+    }public short mathABS(short a){
+        return (short) Math.abs(a);
+    }public byte mathABS(byte a){
+        return (byte) Math.abs(a);
+    }public long mathABS(long a){
+        return Math.abs(a);
+    }
+    //***************** KVADRAT DARAJA ***************************//
+    public double mathPow(double a){
         return Math.pow(a,2);
+    }
+    public int mathPow(int a){
+        return a*a;
+    }
+    public float mathPow(float a){
+        return a*a;
+    }
+    public short mathPow(short a){
+        return (short) (a*a);
+    }
+    public byte mathPow(byte a){
+        return (byte) (a*a);
+    }
+    public long mathPow(long a){
+        return a*a;
     }
 }
